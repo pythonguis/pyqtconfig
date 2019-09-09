5a24e7a6f15d796737ad273a73a316ef0ce8d231
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-09-09 22:29:07 +0200

    Bump version: 0.8.13 → 0.8.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='pyqtconfig',
-    version="0.8.13",
+    version="0.8.14",
     author='Martin Fitzpatrick',
     author_email='martin.fitzpatrick@gmail.com',
     url='https://github.com/mfitzp/pyqtconfig',
