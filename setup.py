56ec7b6eae7cf841843b7306168887e387eaceaa
Miroslav Jirik <miroslav.jirik@gmail.com>
2019-09-10 12:49:46 +0200

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='pyqtconfig',
-    version="0.8.18",
+    version="0.8.19",
     author='Martin Fitzpatrick',
     author_email='martin.fitzpatrick@gmail.com',
     url='https://github.com/mfitzp/pyqtconfig',
