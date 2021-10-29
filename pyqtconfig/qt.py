511da833671a0275edcdfdb5fd36a34658856313
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2021-10-29 15:33:31 +0200

    Add support for PyQt6 & PySide6.

--- pyqtconfig/qt.py
+++ pyqtconfig/qt.py
@@ -1,35 +1,61 @@
 # -*- coding: utf-8 -*-
-''' 
+'''
 This module is a header-like file for pyqtconfig. Handles compatiblity
 with both PyQt5 and PySide2.
 '''
 import sys
 
 if "PyQt5" in sys.modules:
-    from PyQt5.QtCore import (QVariant, Qt, QMutex, QMutexLocker, QSettings,
-                              QObject, pyqtSignal)
-    from PyQt5.QtWidgets import (QComboBox, QCheckBox, QAction,
-                                 QActionGroup, QPushButton, QSpinBox,
-                                 QDoubleSpinBox, QPlainTextEdit, QLineEdit,
-                                 QListWidget, QSlider, QButtonGroup,
-                                 QTabWidget, QApplication, QGridLayout,
-                                 QTextEdit, QWidget, QMainWindow)
     from PyQt5 import QtWidgets
+    from PyQt5.QtCore import (QMutex, QMutexLocker, QObject, QSettings, Qt,
+                              QVariant, pyqtSignal)
+    from PyQt5.QtWidgets import (QAction, QActionGroup, QApplication,
+                                 QButtonGroup, QCheckBox, QComboBox,
+                                 QDoubleSpinBox, QGridLayout, QLineEdit,
+                                 QListWidget, QMainWindow, QPlainTextEdit,
+                                 QPushButton, QSlider, QSpinBox, QTabWidget,
+                                 QTextEdit, QWidget)
 
     Signal = pyqtSignal
 
 elif "PySide2" in sys.modules:
-    from PySide2.QtWidgets import (QComboBox, QCheckBox, QAction, QMainWindow,
-                              QActionGroup, QPushButton, QSpinBox,
-                              QDoubleSpinBox, QPlainTextEdit, QLineEdit,
-                              QListWidget, QSlider, QButtonGroup, QWidget,
-                              QTabWidget, QApplication, QGridLayout, QTextEdit)
-    from PySide2.QtCore import (Signal, Qt, QMutex, QMutexLocker, QSettings,
-                               QObject)
-
     from PySide2 import QtWidgets
-    
+    from PySide2.QtCore import (QMutex, QMutexLocker, QObject, QSettings, Qt,
+                                Signal)
+    from PySide2.QtWidgets import (QAction, QActionGroup, QApplication,
+                                   QButtonGroup, QCheckBox, QComboBox,
+                                   QDoubleSpinBox, QGridLayout, QLineEdit,
+                                   QListWidget, QMainWindow, QPlainTextEdit,
+                                   QPushButton, QSlider, QSpinBox, QTabWidget,
+                                   QTextEdit, QWidget)
+
     QVariant = None
 
+elif "PyQt6" in sys.modules:
+    from PyQt6 import QtWidgets
+    from PyQt6.QtCore import (QMutex, QMutexLocker, QObject, QSettings, Qt,
+                              QVariant, pyqtSignal)
+    from PyQt6.QtGui import QAction, QActionGroup
+    from PyQt6.QtWidgets import (QApplication, QButtonGroup, QCheckBox,
+                                 QComboBox, QDoubleSpinBox, QGridLayout,
+                                 QLineEdit, QListWidget, QMainWindow,
+                                 QPlainTextEdit, QPushButton, QSlider,
+                                 QSpinBox, QTabWidget, QTextEdit, QWidget)
 
+    Signal = pyqtSignal
 
+elif "PySide6" in sys.modules:
+    from PySide6 import QtWidgets
+    from PySide6.QtCore import (QMutex, QMutexLocker, QObject, QSettings, Qt,
+                                Signal)
+    from PySide6.QtGui import QAction, QActionGroup
+    from PySide6.QtWidgets import (QApplication, QButtonGroup, QCheckBox,
+                                   QComboBox, QDoubleSpinBox, QGridLayout,
+                                   QLineEdit, QListWidget, QMainWindow,
+                                   QPlainTextEdit, QPushButton, QSlider,
+                                   QSpinBox, QTabWidget, QTextEdit, QWidget)
+
+    QVariant = None
+
+else:
+    raise ImportError("Qt library must be imported before pyqtconfig.")
